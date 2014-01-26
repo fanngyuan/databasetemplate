878da2201dc79dfcff049b03d30db679901d3d8d
fanngyuan <fanngyuan@gmail.com>
2014-01-26 17:55:19 +0800

    add into array func

--- database_template.go
+++ database_template.go
@@ -2,6 +2,8 @@ package databasetemplate
 
 import (
     "database/sql"
+	"fmt"
+	"reflect"
 )
 
 type DatabaseTemplateImpl struct{
@@ -14,6 +16,7 @@ type DatabaseTemplate interface{
 	Query(sql string,mapRow MapRow,params ...interface{})(object interface{},err error)
 	Exec(sql string,params ...interface{})(err error)
 	QueryArray(sql string,mapRow MapRow,params ...interface{})([]interface{},error)
+	QueryIntoArray(resultList interface{},sql string,mapRow MapRow,params ...interface{})(error)
 	QueryObject(sql string,mapRow MapRow,params ...interface{})(object interface{},err error)
 }
 
@@ -45,6 +48,54 @@ func (this *DatabaseTemplateImpl) QueryArray(sql string,mapRow MapRow,params ...
 	return resArray,nil
 }
 
+func(this *DatabaseTemplateImpl) QueryIntoArray(resultList interface{},sql string,mapRow MapRow,params ...interface{})(error){
+	pointerElements := true
+	t, err := toType(resultList)
+	if err != nil {
+		var err2 error
+		if t, err2 = toSliceType(resultList); t == nil {
+			if err2 != nil {
+				return err2
+			}
+			return  err
+		}
+		pointerElements = t.Kind() == reflect.Ptr
+		if pointerElements {
+			t = t.Elem()
+		}
+	}
+	sliceValue := reflect.Indirect(reflect.ValueOf(resultList))
+	result,err:=this.Conn.Query(sql,params...)
+	defer result.Close()
+	if err!=nil {
+		return err
+	}
+	for result.Next(){
+		v,err:=mapRow(result)
+		if err!=nil{
+			return err
+		}
+		if pointerElements {
+			if reflect.TypeOf(v).Kind()==reflect.Ptr{
+				value:=reflect.ValueOf(v)
+				sliceValue.Set(reflect.Append(sliceValue,value))
+			}else{
+				return fmt.Errorf("can't get struct to pointer array")
+			}
+		}else{
+			if reflect.TypeOf(v).Kind()!=reflect.Ptr{
+				sliceValue.Set(reflect.Append(sliceValue, reflect.ValueOf(v)))
+			}else{
+				return fmt.Errorf("can't get pointer to struct array")
+			}
+		}
+	}
+	if sliceValue.IsNil() {
+		sliceValue.Set(reflect.MakeSlice(sliceValue.Type(), 0, 0))
+	}
+	return nil
+}
+
 func (this *DatabaseTemplateImpl) QueryObject(sql string,mapRow MapRow,params ...interface{})(object interface{},err error){
 	result,error:=this.Conn.Query(sql,params...)
 	defer result.Close()
@@ -64,4 +115,36 @@ func (this *DatabaseTemplateImpl) Exec(sql string,params ...interface{})(err err
 		err=error
 	}
 	return
+}
+
+// toSliceType returns the element type of the given object, if the object is a
+// "*[]*Element" or "*[]Element". If not, returns nil.
+// err is returned if the user was trying to pass a pointer-to-slice but failed.
+func toSliceType(i interface{}) (reflect.Type, error) {
+        t := reflect.TypeOf(i)
+        if t.Kind() != reflect.Ptr {
+                // If it's a slice, return a more helpful error message
+                if t.Kind() == reflect.Slice {
+                        return nil,fmt.Errorf("database_template: Cannot SELECT into a non-pointer slice: %v", t)
+                }
+                return nil, nil
+        }
+        if t = t.Elem(); t.Kind() != reflect.Slice {
+                return nil, nil
+        }
+        return t.Elem(), nil
+}
+
+func toType(i interface{}) (reflect.Type, error) {
+        t := reflect.TypeOf(i)
+
+        // If a Pointer to a type, follow
+        for t.Kind() == reflect.Ptr {
+                t = t.Elem()
+        }
+
+        if t.Kind() != reflect.Struct {
+                return nil, fmt.Errorf("database_template: Cannot SELECT into this type: %v", reflect.TypeOf(i))
+        }
+        return t, nil
 }
---
+++ database_template_test.go
@@ -0,0 +1,87 @@
+package databasetemplate
+
+import (
+    "database/sql"
+	"testing"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+)
+
+type Test struct{
+	A int
+	B string
+}
+
+func TestExec(t *testing.T){
+	db, err := sql.Open("mysql", "root:@/lomo_dev?charset=utf8")
+	defer db.Close()
+	db.SetMaxIdleConns(10)
+	if err!=nil{
+		return
+	}
+	dbTemplate:=&DatabaseTemplateImpl{db}
+	err=dbTemplate.Exec("create table if not exists test(a int not null AUTO_INCREMENT,b varchar(10),primary key(a))")
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+
+	err=dbTemplate.Exec("truncate table test")
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+
+	err=dbTemplate.Exec("insert into  test(b) values(?)","aaa")
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+
+	err=dbTemplate.Exec("insert into  test(b) values(?)","bbb")
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+
+    mapRow := func(resultSet *sql.Rows)(object interface{},err error){
+		t:=Test{}
+		err = resultSet.Scan(&t.A,&t.B)
+		if err!= nil{
+			return nil,err
+		}
+        return t,err
+    }
+
+    mapRowPtr := func(resultSet *sql.Rows)(object interface{},err error){
+		t:=Test{}
+		err = resultSet.Scan(&t.A,&t.B)
+		if err!= nil{
+			return nil,err
+		}
+        return &t,err
+    }
+
+	var tests []Test
+	err=dbTemplate.QueryIntoArray(&tests,"select * from test",mapRow)
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+	fmt.Println(tests)
+
+	var testsPtr []*Test
+	err=dbTemplate.QueryIntoArray(&testsPtr,"select * from test",mapRowPtr)
+	if err!=nil{
+		t.Error("can't get from db",err)
+	}
+	fmt.Println(*testsPtr[0])
+
+	var testsPtr2 []*Test
+	err=dbTemplate.QueryIntoArray(&testsPtr2,"select * from test",mapRow)
+	if err==nil{
+		t.Error("should be error")
+	}
+
+	var tests2 []Test
+	err=dbTemplate.QueryIntoArray(&tests2,"select * from test",mapRowPtr)
+	if err==nil{
+		t.Error("should be error")
+	}
+
+}
